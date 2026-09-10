122a89d4b336bf764dfb990ae7c7123acc93ccd8
Graysen Gould <gouldgraysen@gmail.com>
2026-09-09 22:59:10 -0500

    added about me section content

--- src/commands/about.jsx
+++ src/commands/about.jsx
@@ -1,7 +1,8 @@
 export default function About() {
   return (
     <div className="output-block">
-      <p className="placeholder-text">full bio coming soon.</p>
+      <p className="placeholder-text about-text">Howdy everyone! My name is Graysen, and I am an Undergrad Computational Intelligence Researcher at Texas Tech.
+I love to build things and experiment! In fact, I've built things for both Tyler Technologies and NVIDIA. If you have ideas for more cool things to build, please reach out!</p>
     </div>
   )
 }
