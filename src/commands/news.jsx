c6ba60b28045ff65c0f9ef19805f3a861a617bfa
Graysen Gould <gouldgraysen@gmail.com>
2026-09-09 23:06:23 -0500

    added content to news section

--- src/commands/news.jsx
+++ src/commands/news.jsx
@@ -1,7 +1,10 @@
 export default function News() {
   return (
     <div className="output-block">
-      <p className="placeholder-text">no news yet — check back soon.</p>
+      <p className="placeholder-text">September 2026:</p>
+      <ul style={{ margin: '20px' }}>
+        <li>Joined the CI2 research lab at Texas Tech!</li>
+      </ul>
     </div>
   )
 }
